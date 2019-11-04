86d4eba63a44d3235bbefa775c588b0e86f15814
Anjana S Porawagama <axewilledge123@gmail.com>
2019-11-04 13:51:30 +0530

    changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var mysql = require('mysql');
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -11,39 +13,36 @@ var con = mysql.createConnection({
     database: "broadcast"
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
     if (err) throw err;
     console.log("DB Connected!");
-    
+
 });
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 
-app.get('/server', function(req, res){
-    res.sendFile(__dirname + '/server.html');
+app.get('/serverpush', function (req, res) {
+    con.query("SELECT * FROM candidates", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result[0].name);
+        io.emit('client get', result);
+        res.sendStatus(200);
+    });
 });
 
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('server send', function(msg){
-        console.log('message: ' + msg);
-        con.query("SELECT * FROM candidates", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result[0].name);
-            io.emit('client get', result);
-        });
-    });
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
